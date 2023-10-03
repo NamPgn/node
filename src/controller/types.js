6c860d2f91ad44838126345984b6076b1c318b3b
hihi <101863356+NamPgn@users.noreply.github.com>
2023-10-03 17:59:18 +0000

    Update types.js

--- src/controller/types.js
+++ src/controller/types.js
@@ -1,18 +1,21 @@
 import Types from "../module/types";
-import Products from '../module/products';
+import Products from "../module/products";
 import category from "../module/category";
 
 export const GetAllTypeCategorys = async (req, res) => {
   try {
-    const data = await Types.find().sort({ 'path': 1 }).populate('categorymain.cates').populate('products').populate('category');
+    const data = await Types.find()
+      .sort({ path: 1 })
+      .populate("categorymain.cates")
+      .populate("products")
+      .populate("category");
     return res.status(200).json(data);
   } catch (error) {
     return res.status(400).json({
       message: error.message,
-    })
+    });
   }
-}
-
+};
 
 export const GetOneTypeCategory = async (req, res) => {
   try {
@@ -21,26 +24,25 @@ export const GetOneTypeCategory = async (req, res) => {
     const pageSize = parseInt(req.query.pageSize || "10");
     const skipCount = (page - 1) * pageSize;
     const data = await Types.findById(id)
-      .populate('categorymain.cates')
+      .populate("categorymain.cates")
       .populate({
-        path: 'products',
+        path: "products",
         options: {
           skip: skipCount,
           limit: pageSize,
-        }
+        },
       })
-      .populate('category');
+      .populate("category");
     return res.status(200).json({
       data: data,
-      length: (await Types.findById(id).populate('products')).products.length
+      length: (await Types.findById(id).populate("products")).products.length,
     });
   } catch (error) {
     return res.status(400).json({
       message: error.message,
-    })
+    });
   }
-}
-
+};
 
 export const CreateType = async (req, res) => {
   try {
@@ -50,33 +52,35 @@ export const CreateType = async (req, res) => {
   } catch (error) {
     return res.status(400).json({
       message: error.message,
-    })
+    });
   }
-}
+};
 
 export const DeleteType = async (req, res) => {
   try {
     const id = req.params.id;
     const body = req.body;
-    const s = await Types.findByIdAndUpdate(body.TypeId, { //tìm thằng categorymain
+    const s = await Types.findByIdAndUpdate(body.TypeId, {
+      //tìm thằng categorymain
       $pull: { categorymain: { cates: { $in: [id] } } },
     });
     const d = await Products.findByIdAndDelete(id);
-    const e = await Types.findByIdAndUpdate(body.TypeId, { //tìm thằng categorymain
+    const e = await Types.findByIdAndUpdate(body.TypeId, {
+      //tìm thằng categorymain
       $pull: { products: { $in: [id] } },
     });
     return res.json({
       success: true,
       dataProduct: d,
       dataCateMain: s,
-      productType: e
+      productType: e,
     });
   } catch (error) {
     return res.status(400).json({
       message: error.message,
-    })
+    });
   }
-}
+};
 
 export const UpdatedType = async (req, res) => {
   try {
@@ -87,9 +91,9 @@ export const UpdatedType = async (req, res) => {
   } catch (error) {
     return res.status(400).json({
       message: error.message,
-    })
+    });
   }
-}
+};
 
 export const pushCategory = async (req, res) => {
   try {
@@ -98,25 +102,29 @@ export const pushCategory = async (req, res) => {
     const data = await category.findById(body.categoryId);
     const newData = await Types.findByIdAndUpdate(typeId, {
       $addToSet: { category: data },
-    })
+    });
     res.json({
       success: true,
-      data: newData
-    })
+      data: newData,
+    });
   } catch (error) {
     return res.status(400).json({
       message: error.message,
-    })
+    });
   }
-}
+};
 
 export const getPhim = async (req, res) => {
   try {
-    const data = await Types.findOne({ name: "Phim lẻ" }).populate('categorymain.cates').populate('products').populate('category')
-    return res.status(200).json(data)
+    const key = req.query.key;
+    const data = await Types.findOne({ name: key })
+      .populate("categorymain.cates")
+      .populate("products")
+      .populate("category");
+    return res.status(200).json(data);
   } catch (error) {
     return res.status(400).json({
       message: error.message,
-    })
+    });
   }
-}
+};
