d561a4fd5243f7eacc7f2cc02551805a7b7c0838
hihi <101863356+NamPgn@users.noreply.github.com>
2023-10-20 09:06:44 +0000

    Update 4 files

--- src/module/category.js
+++ src/module/category.js
@@ -1,33 +1,47 @@
 import mongoose from "mongoose";
 
-const categorySchema = new mongoose.Schema({
-  name: {
-    type: String
+const categorySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+    },
+    linkImg: {
+      type: String,
+    },
+    des: {
+      type: String,
+    },
+    sumSeri: {
+      type: String,
+    },
+    products: [
+      {
+        type: mongoose.Types.ObjectId,
+        ref: "Products",
+      },
+    ],
+    type: {
+      type: String,
+    },
+    week: {
+      type: mongoose.Types.ObjectId,
+      ref: "Week",
+    },
+    up: {
+      type: Number,
+    },
+    year: {
+      type: String,
+    },
+    time: {
+      type: String,
+    },
+    isActive: {
+      type: Number,
+      default: 0,
+    },
   },
-  linkImg: {
-    type: String
-  },
-  des: {
-    type: String
-  },
-  sumSeri: {
-    type: String
-  },
-  products: [
-    {
-      type: mongoose.Types.ObjectId, ref: 'Products'
-    }
-  ],
-  type: {
-    type: String
-  },
-  week: {
-    type: mongoose.Types.ObjectId,
-    ref: 'Week'
-  },
-  up: {
-    type: Number
-  }
-}, { timestamps: true });
+  { timestamps: true },
+);
 categorySchema.indexes();
 export default mongoose.model("Category", categorySchema);
